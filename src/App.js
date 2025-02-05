586139a1a3b033723df501df04ea134b9520e492
Baptista-Paulo-Joaquim <baptistajoaquim0@gmail.com>
2025-02-05 12:16:47 +0200

    new features underway

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from "react";
-import "./styles.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import Home from "./elements/Home"
-import Create from "./elements/Create"
-import Read from "./elements/Read"
-import Update from "./elements/Update"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/read/:id" element={<Read /> } />
-        <Route path="/update/:id" element={<Update />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1>ReactJS!</h1>
+    </>
   );
 }
 
